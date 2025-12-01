e793d4d5b406501b2e99183c7c8f336f08ff492f
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-12-01 23:27:02 +0100

    Rework part 1 to it's own function with input handling first

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::io::{self, Result};
 
 fn main() -> Result<()> {
+    let input: Vec<String> = io::stdin().lines().map(|line| line.unwrap()).collect();
+    p1(&input)?;
+    Ok(())
+}
+
+fn p1(input: &Vec<String>) -> Result<()> {
     let mut position = 50;
     let mut zeroes = 0;
-    let input = io::stdin().lines();
 
     for op in input {
-        let op = op.unwrap();
         let (dir, mov) = op.split_at(1);
         // The choice of i16 is to handle the 99 + 99 possibility which is too large to fit in i8
         let mov = mov.parse::<i16>().expect("Expected move to be within 255 range, as the dial only goes 0 -> 99") % 100;
