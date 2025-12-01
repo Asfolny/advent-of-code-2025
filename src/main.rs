d46c21a503ecd0769a19aaf50016d3c4d817b5a3
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-12-01 20:08:07 +0100

    Naive main that runs off of the example

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::io::{self, Result};
+
+fn main() -> Result<()> {
+    let mut position = 50;
+    let mut zeroes = 0;
+    let input = vec!["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"];
+
+    for op in input {
+        let op = op.unwrap();
+        let (dir, mov) = op.split_at(1);
+        // The choice of i16 is to handle the 99 + 99 possibility which is too large to fit in i8
+        let mov = mov.parse::<i16>().expect("Expected move to be within 255 range, as the dial only goes 0 -> 99") % 100;
+
+        position += match dir {
+            "L" => -mov,
+            "R" => mov,
+            _ => panic!("Unknown operation"),
+        };
+        position %= 100;
+
+        if position < 0 {
+            position += 100;
+        }
+
+        if position == 0 {
+            zeroes += 1;
+        }
+
+    }
+    println!("Zero count: {zeroes}");
+    Ok(())
+}
