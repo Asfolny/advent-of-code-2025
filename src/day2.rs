1933030b4fe7580ef605fb8b200cf0f5c5b743f7
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-12-02 20:55:47 +0100

    Created test and bare fn

--- src/day2.rs
+++ src/day2.rs
@@ -6,7 +6,8 @@ pub fn p1(input: &str) -> u64 {
             .split('-')
             .take(2)
             .map(|p| {
-                p.trim().parse::<u64>()
+                p.trim()
+                    .parse::<u64>()
                     .expect("Each side of the range must be a valid u64 number")
             })
             .collect::<Vec<_>>()
@@ -28,13 +29,25 @@ pub fn p1(input: &str) -> u64 {
     result
 }
 
+pub fn p2(input: &str) -> u64 {
+    let mut result: u64 = 0;
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn sample() {
+    fn sample_p1() {
         let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
         assert_eq!(p1(input), 1227775554)
+    }
+
+    #[test]
+    fn sample_p2() {
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        assert_eq!(p2(input), 4174379265)
     }
 }
