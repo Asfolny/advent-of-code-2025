8cc456809b9056523a4a949a9c55ca0b51059681
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-12-02 18:52:20 +0100

    Add day 2 part 1

---
+++ src/day2.rs
@@ -0,0 +1,40 @@
+pub fn p1(input: &str) -> u64 {
+    let mut result: u64 = 0;
+
+    for range in input.split(',') {
+        let [first, last]: [u64; 2] = range
+            .split('-')
+            .take(2)
+            .map(|p| {
+                p.trim().parse::<u64>()
+                    .expect("Each side of the range must be a valid u64 number")
+            })
+            .collect::<Vec<_>>()
+            .try_into()
+            .expect("Could not find left and right sides of <l>-<r> format");
+
+        for p in first..=last {
+            let pstr = p.to_string();
+            if pstr.len() % 2 == 1 {
+                continue;
+            } // odd number, always valid
+            let (high, low) = pstr.split_at(pstr.len() / 2);
+            if low == high {
+                result += p;
+            }
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample() {
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        assert_eq!(p1(input), 1227775554)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,15 @@ use std::fs;
 use std::io::{Result};
 
 mod day1;
+mod day2;
 
 pub fn run() -> Result<()> {
     let input = fs::read_to_string("../res/input-1.txt")?;
     day1::p1(&input);
     day1::p2(&input);
+
+    let input = fs::read_to_string("../res/input-2.txt")?;
+    println!("Invalid total: {}", day2::p1(&input));
 
     Ok(())
 }
